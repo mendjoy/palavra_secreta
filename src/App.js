5e1cb7a41c7d2744a3d21f12a307012a05ade5d9
Joyce M Miranda <106771419+mendjoy@users.noreply.github.com>
2022-09-24 20:18:28 -0300

    adicionando componentes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-
+//CSS
 import './App.css';
+
+//REACT 
+import {useCallBack, useEffect, useState} from "react"; 
+
+//data
+import { wordList } from './data/word';
+
+//components
 import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+  {id:1, name: 'start'},
+  {id:2, name:'game'},
+  {id:3, name:'gameover'}
+]
+
 
 function App() {
+  const[gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordList);
+
+  console.log(words)
   return (
     <div className="App">
-     <StartScreen/>
+     {gameStage === 'start' && <StartScreen/>}
+     {gameStage === 'game' && <Game/>}
+     {gameStage === 'gameover' && <GameOver/>}
     </div>
   );
 }
