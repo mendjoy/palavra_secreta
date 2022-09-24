aad6a5507400b11b89bd9828b04cfa38d2f3d62c
Joyce M Miranda <106771419+mendjoy@users.noreply.github.com>
2022-09-24 20:04:59 -0300

    configurações iniciais

---
+++ src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import './StartScreen.css';
+
+const StartScreen = () => {
+  return (
+    <div className='start'>
+        <h1>Palavra Secreta</h1>
+        <p>Clique no botão abaixo para iniciar.</p>
+        <button>Iniciar</button>
+    </div>
+  )
+}
+
+export default StartScreen
---
+++ src/data/word.js
@@ -0,0 +1,7 @@
+export const wordList = {
+    fruta: ['Banana', 'Maça', 'Melancia', 'Laranja', 'Goiaba', 'Melão', 'Pessego'],
+    corpo: ['Braço', 'Perna', 'Cerebro', 'Meninge', 'Faringe', 'Tendão', 'Medula Espinhal'],
+    inseto: ['Abelha', 'Vespa', 'Cupim', 'Gafanhoto', 'Percevejo', 'Joaninha', 'Formiga'],  
+};
+
+
