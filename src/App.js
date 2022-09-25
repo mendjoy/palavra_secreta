ca3d04f1fc71d289199df8ba18aa6faec40b2018
Joyce M Miranda <106771419+mendjoy@users.noreply.github.com>
2022-09-24 21:59:12 -0300

    inicio do jogo e seu estilo

--- src/App.js
+++ src/App.js
@@ -22,10 +22,42 @@ const stages = [
 function App() {
   const[gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordList);
+  const [pickedWord, setPickedWord] = useState('');
+  const  [pickedCategory, setPickedCategory] = useState('');
+  const [letters, setLetters] = useState([]);
 
+  //escolher categoria aleatoria 
+  const pickWordAndCategory = () => {
+    const categories = Object.keys(words)
+    const category  = categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+     //escolher uma palavra aleatoria 
+    const word = words[category][Math.floor(Math.random() * words[category].length)];
+
+    return{word, category};
+   
+  };
+
+ 
+  
+   
+   
 //Inicia o jogo
   const startGame = () =>{
-    setGameStage(stages[1].name)
+    // escolher palavra e categoria
+    const {word, category} = pickWordAndCategory();
+
+    //array de letras
+    let wordLetters = word.split('');
+    wordLetters = wordLetters.map((l) => l.toLowerCase()); 
+    
+    //preencher states 
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(letters);
+
+
+    setGameStage(stages[1].name);
   };
 
 //letra input
@@ -37,6 +69,8 @@ function App() {
   const retry = () => {
     setGameStage(stages[0].name)
   }
+
+  
 
 
   return (
